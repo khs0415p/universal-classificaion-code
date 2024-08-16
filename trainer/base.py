488adc26d8e4dc2dc2914d0aa0fcd399933575db
Hyunsoo Kim <khs0415p@naver.com>
2024-08-16 13:36:00 +0900

    Feat: Save confusion matrix

--- trainer/base.py
+++ trainer/base.py
@@ -343,8 +343,9 @@ class BaseTrainer:
             preds.extend(pred)
             labels.extend(label)
 
-        save_confusion_matrix(preds, labels, self.config.model_type, self.config.checkpoint)
-        f1, acc = get_metrics(preds, labels)
+        
+        f1, acc, cm = get_metrics(preds, labels)
+        save_confusion_matrix(labels, cm, self.config.model_type, self.config.checkpoint)
         LOGGER.info(f"{'F1-score':<15}{f1}")
         LOGGER.info(f"{'Accuracy':<15}{acc}")
 
--- utils/metric_utils.py
+++ utils/metric_utils.py
@@ -1,29 +1,47 @@
 import json
 import seaborn as sns
+import pandas as pd
 import matplotlib.pyplot as plt
 
 from sklearn.metrics import accuracy_score, f1_score, confusion_matrix
 
-def save_confusion_matrix(preds, labels, model_name='', checkpoint=''):
 
+def apply_format(row, df_ratio):
+    output = []
+    
+    for v, r in zip(row, df_ratio.loc[row.name]):
+        output.append(f"{v} ({r:.4f})")
+    
+    return output
+
+def save_confusion_matrix(labels, confusion_matrix, model_name='', checkpoint=''):
     with open("data/label2id.json", "r") as f:
         label2id = json.load(f)
     id2label = {v:k for k, v in label2id.items()}
     classes = list(map(lambda x:id2label[x], sorted(set(labels))))
 
-    cm = confusion_matrix(labels, preds)
-
     plt.figure(figsize=(8, 6))
-    sns.heatmap(cm, annot=True, fmt='d', cmap='Blues', xticklabels=classes, yticklabels=classes)
+    sns.heatmap(confusion_matrix, annot=True, fmt='d', cmap='Blues', xticklabels=classes, yticklabels=classes)
     plt.xticks(rotation=90)
     plt.yticks(rotation=0)
     plt.xlabel('Predicted')
     plt.ylabel('Actual')
     plt.title(f"Confusion Matrix - {model_name}")
-    
 
     plt.savefig(f"{checkpoint}/confusion_matrix_{model_name}.png", bbox_inches='tight', dpi=300)
 
+    # confusion matrix
+    confusion_df = pd.DataFrame(confusion_matrix, columns=classes, index=classes)
+
+    df_sum = confusion_df.sum(axis=1)
+    df_ratio = confusion_df.div(df_sum, axis=1)
+
+    df_formatted = confusion_df.apply(apply_format, df_ratio=df_ratio, axis=1)
+    df_formatted = pd.DataFrame(df_formatted.tolist(), columns=confusion_df.columns, index=confusion_df.index)
+
+    df_formatted.to_csv(f"{checkpoint}/confusion_matrix_{model_name}.csv")
+
 
 def get_metrics(preds, labels):
-    return f1_score(labels, preds, average="weighted"), accuracy_score(labels, preds)
+
+    return f1_score(labels, preds, average="weighted"), accuracy_score(labels, preds), confusion_matrix(labels, preds)
